03bbe51383bbedf9052f6aee5aa7a225e46fd9cd
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-05-03 20:13:16 -0300

    DbContext Created

---
+++ EmployeeSystem/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeSystem.Models;
+
+namespace EmployeeSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            public DbSet<Employee> Employees => Set<Employee>();
+        }        
+    }
+}
