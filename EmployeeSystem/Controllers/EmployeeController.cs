8d100063122af3c8ecb476b983088071fe42ac69
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-05-03 20:45:11 -0300

    Adding Employee Empty Controller

---
+++ EmployeeSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public FuncionariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
