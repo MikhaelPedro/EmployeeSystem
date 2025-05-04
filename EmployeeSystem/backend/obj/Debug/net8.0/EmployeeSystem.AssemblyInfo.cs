05c0a51250277972d8a3a26d28a951cffacee3bd
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-05-04 19:29:43 -0300

    Added new folder backend to separate front angular and back c#

--- EmployeeSystem/backend/obj/Debug/net8.0/EmployeeSystem.AssemblyInfo.cs
+++ EmployeeSystem/backend/obj/Debug/net8.0/EmployeeSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c377ef797e0cd09c4720b2732f79c67bfc55ea8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc7d9a9914ad908f7c60ed676691d21650a278a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
