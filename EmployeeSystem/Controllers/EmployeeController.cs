5f9ae14a08835cdd6a5497389a5a02e6934c2d56
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-05-03 22:53:34 -0300

    Adding new route DeleteWmployee

--- EmployeeSystem/Controllers/EmployeeController.cs
+++ EmployeeSystem/Controllers/EmployeeController.cs
@@ -37,6 +37,7 @@ namespace EmployeeSystem.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
         }
+
         [HttpPut("{id}")]
         public async Task<IActionResult> PutEmployee(int id, Employee employee)
         {
@@ -44,6 +45,16 @@ namespace EmployeeSystem.Controllers
             _context.Entry(employee).State = EntityState.Modified;
             await _context.SaveChangesAsync();
             return NoContent();
-        }        
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null) return NotFound();
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }                
     }
 }
