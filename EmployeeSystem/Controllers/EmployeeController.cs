b470e5176a34832bdab8bf0524f77ae618a00ea4
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-05-03 22:45:39 -0300

    Adding new route PostEmployee

--- EmployeeSystem/Controllers/EmployeeController.cs
+++ EmployeeSystem/Controllers/EmployeeController.cs
@@ -29,5 +29,13 @@ namespace EmployeeSystem.Controllers
             if (employee == null) return NotFound();
             return employee;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
     }
 }
